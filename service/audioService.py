ba038a5963e6b6bcb8c4ea76eb8607aaadb38bb2
kotchaporns <6322772292@g.siit.tu.ac.th>
2023-11-30 15:16:47 +0700

    update cli

--- service/audioService.py
+++ service/audioService.py
@@ -11,18 +11,21 @@ from pydub import AudioSegment
 
 from .modelService import predictModel
 
+s3 = boto3.client("s3")
+bucketname = "snorewisebucket"
 
-s3 = boto3.client(
-   "s3",
-   aws_access_key_id="ASIAU6Y6O34M7ZYK65GO",
-   aws_secret_access_key="WgkJSILBSNjDtf+BEY9OU7tPbey1TCx8fNDEK2w7",
-   aws_session_token="FwoGZXIvYXdzEPj//////////wEaDJjdm9jxn/xPQ0UXwyLLAWQyege+0P53O+PDiUhGCxEmzmspIP32vwWD0HgVf5IVkVETYwxBrez7U+ZiezFwqIPV6b3kE1KxgQ9QsBeSvfkkcStedzRGxuDPu0kwJ0Pyns4nUQQO8agU1RPwt06jBgNr1vEDLCi30UkAsA9tDlt9rzs1WJxVJlD8oIKmxEipJdzaMeGpLSVhtVr5bkOvUET583K91sgJm8zUJYud/ex+fNDCs8XuOXIMU9/9fAVJZfxsGd0Kw2VVBYwHXbNOqjKJp1B+avfqhv1QKL7l+6oGMi0+gWElnUnMDhsACRIpv5qpKG997hc+gK9hHEPUFLZrc3+ZqAlItz6jwCpuuoU="
-)
+# s3 = boto3.client(
+#    "s3",
+#    aws_access_key_id="ASIAU6Y6O34M7ZYK65GO",
+#    aws_secret_access_key="WgkJSILBSNjDtf+BEY9OU7tPbey1TCx8fNDEK2w7",
+#    aws_session_token="FwoGZXIvYXdzEPj//////////wEaDJjdm9jxn/xPQ0UXwyLLAWQyege+0P53O+PDiUhGCxEmzmspIP32vwWD0HgVf5IVkVETYwxBrez7U+ZiezFwqIPV6b3kE1KxgQ9QsBeSvfkkcStedzRGxuDPu0kwJ0Pyns4nUQQO8agU1RPwt06jBgNr1vEDLCi30UkAsA9tDlt9rzs1WJxVJlD8oIKmxEipJdzaMeGpLSVhtVr5bkOvUET583K91sgJm8zUJYud/ex+fNDCs8XuOXIMU9/9fAVJZfxsGd0Kw2VVBYwHXbNOqjKJp1B+avfqhv1QKL7l+6oGMi0+gWElnUnMDhsACRIpv5qpKG997hc+gK9hHEPUFLZrc3+ZqAlItz6jwCpuuoU="
+# )
 
 def predictSnore(file):
      # upload_data =  s3.upload_fileobj(file, "snorewisebucket", file.filename)
     audio_stream = file.stream
     yhat, calls = predictModel(audio_stream)
+    upload_data =  s3.upload_fileobj(file, bucketname, file.filename)
     return  yhat, calls
 
 
